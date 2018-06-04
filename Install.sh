b6814676505f4617f8bd873b5ddef40c19ad42aa
Henry Qin <root@hq6.me>
2018-06-04 10:48:24 -0700

    Add the source command when installing to gdbinit

--- Install.sh
+++ Install.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FULLPATH="$DIR/ShellPipeCommand.py"
 present=$(grep "$FULLPATH" $HOME/.gdbinit 2> /dev/null)
 if [[ -z "$present" ]]; then
-    echo "$FULLPATH"  >> ~/.gdbinit
+    echo "source $FULLPATH"  >> ~/.gdbinit
 fi
